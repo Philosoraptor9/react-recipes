b2e6a5158b93527b222598c686701f4810a39819
Philip Hoover <philhoov90@gmail.com>
2018-11-04 16:13:46 -0700

    routes setup, time to test

--- recipes-json-api/db/db.js
+++ recipes-json-api/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb://localhost/musicTheory'
+const connectionString = 'mongodb://localhost/recipeBook'
 
 mongoose.connect({ useNewUrlParser: true });
 
