a1954ecd53fa84798cf2b0e360d828be3d321247
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-11-05 11:31:01 -0700

    create component added

--- react-recipes/src/CreateRecipe/CreateRecipe.jsx
+++ react-recipes/src/CreateRecipe/CreateRecipe.jsx
@@ -18,22 +18,19 @@ class CreateRecipe extends Component {
         }
     render(){
         return(
-            <form>
-                <label>
-                    Title:
-                </label>
-                <br />
-                <label>
-                    Ingredients:
-                </label>
-                <br />
-                <label>
-                    Instructions:
-                </label>
-            
-            </form>
-        )
-    }
+            <Segment>
+                <h4>Create A New Recipe</h4>
+                <Form onSubmit={this.props.addRecipe.bind(null, this.state)}>
+                <Label htmlFor="name=title">Recipe:</Label>
+                <Form.Input type='text' name='title' value={this.state.title} onChange={this.updateRecipe}/>
+                <Label>Ingredients:</Label>
+                <Form.Input type='text' name='ingredients' value={this.state.ingredients} onChange={this.updateRecipe}/>
+
+                <Button color="green" type='Submit'>Create Recipe</Button>
+                </Form>
+      </Segment>
+      )
+  }
 }
 
 export default CreateRecipe;
