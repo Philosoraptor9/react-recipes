5a20d5ae060089bf8b165f718adab6ce36b02fe3
Philip Hoover <philhoov90@gmail.com>
2018-11-07 13:51:58 -0700

    rendering images of recipes

--- react-recipes/src/Components/SearchRecipeItem.jsx
+++ react-recipes/src/Components/SearchRecipeItem.jsx
@@ -4,8 +4,12 @@ const SearchRecipeItem = (props) => {
 console.log(props);
     return(
         <li>
+            <img src={props.recipe.recipe.image} />
+            <br />
             <p><a href={props.recipe.recipe.url}>{props.recipe.recipe.label}</a></p>
+            <br />
             <p>{props.recipe.recipe.healthLabels}</p>
+            <br />
         </li>
     )
 }
