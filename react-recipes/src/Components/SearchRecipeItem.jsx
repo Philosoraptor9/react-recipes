931714ad80d9e845a2ca16a986bb50feefce03cd
Philip Hoover <philhoov90@gmail.com>
2018-11-07 13:25:26 -0700

    tweaking API rendering

--- react-recipes/src/Components/SearchRecipeItem.jsx
+++ react-recipes/src/Components/SearchRecipeItem.jsx
@@ -4,9 +4,8 @@ const SearchRecipeItem = (props) => {
 console.log(props);
     return(
         <li>
-            <p>{props.recipe.recipe.label}</p>
+            <p><a href={props.recipe.recipe.url}>{props.recipe.recipe.label}</a></p>
             <p>{props.recipe.recipe.healthLabels}</p>
-            <p><a href={props.recipe.recipe.url}></a></p>
         </li>
     )
 }
